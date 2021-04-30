42d36be21addb506044dc98d60eb905f8ec91773
snoor_madih <snoor.madih@gmail.com>
2021-04-30 09:03:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+  return fetch("https://anapioficeandfire.com/api/books");
 }
+ 
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+function renderBooks(json) {
+  
+const main = document.querySelector("main");
+  json.forEach(book => {
+    const h2 = document.createElement("h2");
+    h2.innerHTML = `<h2>${book.name}</h2>`;
     main.appendChild(h2);
   });
 }
